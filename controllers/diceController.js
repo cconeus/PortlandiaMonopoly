2b7049fb3e2f1f452780570661b1c19d3fba9641
Dana Sharman <student@epicodus.com>
2015-09-30 16:04:15 -0700

    basic dice function complete, final product not ready yet.

--- controllers/diceController.js
+++ controllers/diceController.js
@@ -1,3 +1,5 @@
-portlandiaMonopoly.controller("DiceCtrl", function DiceCtrl($scope, $stateParams, UtilitiesFactory, GameFactory) {
-  
-})
+portlandiaMonopoly.controller("DiceCtrl", function DiceCtrl($scope, GameFactory) {
+  $scope.diceRoll = function() {
+    return Math.floor((Math.random()*6)+1);
+  }
+});
